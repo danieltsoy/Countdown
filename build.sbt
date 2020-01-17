ed1415555ff4a997d259fb9bfeac351b35aa4489
dtsoy <dttsoy@gmail.com>
2020-01-17 09:28:33 -0500

    protobuf fam

--- build.sbt
+++ build.sbt
@@ -1,6 +1,15 @@
-name := "Countdown"
-scalaVersion := "2.11.8"
+import scalapb.compiler.Version.scalapbVersion
 
-addSbtPlugin("com.thesamet" % "sbt-protoc" % "0.99.27")
-libraryDependencies += "com.softwaremill.sttp.client" %% "core" % "2.0.0-RC6"
-libraryDependencies += "com.thesamet.scalapb" %% "compilerplugin" % "0.9.4"
+scalaVersion := "2.12.10"
+sbtVersion := "1.3.7"
+
+PB.targets in Compile := Seq(
+  scalapb.gen() -> (sourceManaged in Compile).value
+)
+
+libraryDependencies ++= Seq(
+  // For finding google/protobuf/descriptor.proto
+  "com.thesamet.scalapb" %% "scalapb-runtime" % scalapbVersion % "protobuf",
+  "com.softwaremill.sttp.client" %% "core" % "2.0.0-RC6",
+  "com.google.transit" % "gtfs-realtime-bindings" % "0.0.2"
+)
--- src/main/scala/Run.scala
+++ src/main/scala/Run.scala
@@ -1,18 +1,37 @@
 import sttp.client._
+import java.net.{HttpURLConnection, URL}
+
+import com.google.transit.realtime.GtfsRealtime.FeedMessage
+import com.google.transit.realtime.nyc_subway.{NycSubwayProto, NyctFeedHeader, NyctStopTimeUpdate}
+
+import scala.collection.JavaConverters._;
+
 
 object Run extends App {
   // the `query` parameter is automatically url-encoded
   // `sort` is removed, as the value is not defined
-  val request = basicRequest.get(uri"http://datamine.mta.info/mta_esi.php?key=d0443dbe928081a6b831e48bede8f2cf")
+  //  val request = basicRequest.get(uri"https://api-endpoint.mta.info/Dataservice/mtagtfsfeeds/nyct%2Fgtfs-nqrw")
+  //    .header("x-api-key", "RUjE30Aiht4LjlhOnx2sz7TpEFBUAFI46hBlNpBi")
+  //
+  //
+  //  implicit val backend = HttpURLConnectionBackend()
+  //  val response = request.send()
+  //
+  //  // response.header(...): Option[String]
+  //  println(response.header("Content-Length"))
+  //
+  //  // response.body: by default read into an Either[String, String] to indicate failure or success
+  //  println(response.body)
+  //
 
-  implicit val backend = HttpURLConnectionBackend()
-  val response = request.send()
+  val url = new URL("https://api-endpoint.mta.info/Dataservice/mtagtfsfeeds/nyct%2Fgtfs-nqrw")
+  val conn = url.openConnection()
+  conn.setRequestProperty("x-api-key", "RUjE30Aiht4LjlhOnx2sz7TpEFBUAFI46hBlNpBi")
 
-  // response.header(...): Option[String]
-  println(response.header("Content-Length"))
-
-  // response.body: by default read into an Either[String, String] to indicate failure or success
-  println(response.body)
+    val feed = FeedMessage.parseFrom(conn.getInputStream)
+  //  feed.getEntityList.asScala.foreach(entity => if(entity.hasTripUpdate) println(entity.getTripUpdate))
+  //
 
 
+  println(NyctStopTimeUpdate.parseFrom(conn.getInputStream))
 }
